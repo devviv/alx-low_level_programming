c4ad48fad9bfbba6cb0f7efa4a798e1fc57bbc93
devviv <devviv>
2022-09-08 16:03:25 -0700

    C programming with UNIX

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-/**Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
-*Use the function puts
-*You are not allowed to use printf
-*Your program should end with the value 0
+/**
+ * This programme consist to to display a sentence using puts function
+*This is the code below
 */
+
 #include <stdio.h>
 int main(void)
 {
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * Programme to print on screen using printf function
+ */
+
+#include <stdio.h>
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
