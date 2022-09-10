a02430110a0b1f685c44f4894a8efe1da6475ae9
devviv <devviv>
2022-09-09 23:04:54 -0700

    Task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,19 @@
 /**
- *main- is entry
- *return-it will return zero if there is any troubles
+ *main-is the entry
+ *
+ *Return: it will return zero if there is any troubles
+*/
 #include <stdio.h>
 #include <stdlib.h>
-int main ()
+int main(void)
 {
-char A[] = "abcdefghijklmnopqrstuvwxyz ";
+char A[] = "abcdefghijklmnopqrstuvwxyz";
 char *p;
 int i;
 p = A;
-for (i = 0;i <= 25;i++)
+for (i = 0 ; i <= 25 ; i++)
 {
-putchar(*p+i);
+putchar(*p + i);
 }
-putchar('\n');
-return (0) ;
+return (0);
 }
