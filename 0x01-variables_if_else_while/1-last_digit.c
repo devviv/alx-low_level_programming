96f9bf0974d4cf0db9648d7b64f56b0d2bf6e4e7
devviv <devviv>
2022-09-10 14:59:03 -0700

    some tasks

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,7 @@
 /**
  *main- is the entry
  *
- * return:zero
+ *Return:zero
 */#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -10,6 +10,17 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
+if (n % 10 > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+}
+else if (n % 10 == 0)
+{
+printf("Last digit of %d is %d and is 0", n, n % 10);
+}
+else
+{
+printf("Last digit of %d is %d and is less than 6 not 0", n, n % 10);
+}
 return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,15 +2,19 @@
  *main-is the entry
  *
  *Return: it will return zero if there is any troubles
-*/#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+*/
+#include <stdio.h>
 int main(void)
 {
 int i;
-for (i = 0; i < 10; i++)
+for (i = 0 ; i <= 9 ; i++)
 {
 putchar((i % 10) + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
