ceeab5f69024e354004a88fb04205078ac400ead
devviv <devviv>
2022-09-13 15:20:53 -0700

    some other tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+/**
+ * _abs - this program prints laphabet follow by a new line
+ *
+ * @num: character value
+ *
+ * Return: num
+ */
+int _abs(int num)
+{
+int num;
+if (num < 0)
+{
+num = -num;
+return (num);
+}
+else
+{
+num = num;
+return (num);
+}
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_last_digit - this program prints last digit of a number
+ *
+ * @num: character value
+ *
+ * Return: num
+ */
+int main(void)
+{
+times_table();
+return (0);
+}
+void times_table(void)
+{
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+_putchar(i * j);
+}
+putchar('\n');
+}
+}
