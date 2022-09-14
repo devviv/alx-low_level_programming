c20815a25a98e577a027aef78601f3c5f39a077e
devviv <devviv>
2022-09-14 01:48:34 -0700

    tasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,19 +7,19 @@
  *
  * Return: minutes
  */
-void print_to_98(int n)
+oid print_to_98(int n)
 {
 int i;
 if (n < 98)
 {
-for (i = n; n < 98; n++)
+for (i = n; i < 98; i++)
 {
 printf("%d, ", i);
 }
 printf("98");
 printf("\n");
 }
-else if (n > 98)
+else if ( n > 98)
 {
 for (i = n; i > 98; i--)
 {
