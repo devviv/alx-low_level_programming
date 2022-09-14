deb55305b402b5db94bba8d3aabf7d6d895971f6
devviv <devviv>
2022-09-14 14:05:15 -0700

    End

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,18 +12,36 @@ for (i = 0; i < 3; i++)
 {
 if (i < 2)
 {
-for (j = 0; j < 9; j++)
+for (j = 0; j < 10; j++)
 {
 for (k = 0; k < 6; k++)
 {
 for (l = 0; l < 10; l++)
 {
-_putchar(i + '0');
-_putchar(j + '0');
-_putchar(':');
-_putchar(k + '0');
-_putchar(l + '0');
-printf("\n");
+putchar(i + '0');
+putchar(j + '0');
+putchar(':');
+putchar(k + '0');
+putchar(l + '0');
+putchar('\n');
+}
+}
+}
+}
+else
+{
+for (j = 0; j < 4; j++)
+{
+for (k = 0; k < 6; k++)
+{
+for (l = 0; l < 10; l++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(':');
+putchar(k + '0');
+putchar(l + '0');
+putchar('\n');
 }
 }
 }
