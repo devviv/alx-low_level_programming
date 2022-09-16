e426d3347d41df1d767109d0d7288aff39b4d7cb
devviv <devviv>
2022-09-16 13:16:14 -0700

    Task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - this program prints line
+ *
+ *@n: character value
+ *
+ *
+ * Return: num
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+for (e = 0; e < l; e++)
+{
+if (n > 0)
+{
+_putchar(' ');
+}
+}
+_putchar('\\');
+_putchar('\n');
+}
+_putchar('\n');
+}
