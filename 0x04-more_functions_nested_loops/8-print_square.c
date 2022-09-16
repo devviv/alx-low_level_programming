ec8eefb03278562b1b81668697bb4e773c69af3c
devviv <devviv>
2022-09-16 15:23:10 -0700

    8 abd 9

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - this program prints square
+ *
+ *@size: value
+ *
+ *
+ * Return: num
+ */
+void print_square(int size)
+{
+int i;
+for (i = 0; i < size; i++)
+{
+for (i = 0; i < size; i++)
+{
+if (size > 0)
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
