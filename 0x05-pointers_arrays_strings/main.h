3cf9da01b6977b499e230554e603bffe38920f6b
devviv <devviv>
2022-09-19 13:51:36 -0700

    task 0 - 4

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,9 +5,9 @@
  * _putchar - writes the character c to stdout     * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and error is set appropriately.
  */
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
