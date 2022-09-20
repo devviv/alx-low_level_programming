fb2c74fb3265fd87e877d93dc178c97e8ac4eaf8
devviv <devviv>
2022-09-20 16:37:29 -0700

    some tasks

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,21 +4,15 @@
 *@n: value
 * Return: nothing
 */
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 void print_array(int *a, int n)
 {
 int i = 0;
-while (i < n)
-{
-if (i < n - 1)
+while (i < n - 1)
 {
 printf("%d, ", a[i]);
-}
-else
-{
-printf("%d", a[i]);
-}
 i++;
 }
+printf("%d", a[i]);
 }
