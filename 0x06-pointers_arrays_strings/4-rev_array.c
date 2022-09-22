4692c41782aefe05b930fe7b167f9da7177fe8a1
devviv <devviv>
2022-09-22 08:20:58 -0700

    corrections

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,15 +7,15 @@
 #include "main.h"
 void reverse_array(int *a, int n)
 {
-int i, j, Tab[n];
+int i, j, tab[n];
 j = 0;
 for (i = (n - 1); i >= 0; i--)
 {
-Tab[j] = a[i];
+tab[j] = a[i];
 j++;
 }
 for (i = 0; i < n; i++)
 {
-a[i] = Tab[i];
+a[i] = tab[i];
 }
 }
