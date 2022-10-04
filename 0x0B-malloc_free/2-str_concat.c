88a69ebc0cfd71f80a7ae3c0aab9d0d63c393d4b
devviv <devviv>
2022-10-04 16:30:51 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,32 +11,43 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr;
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	if (s1 != NULL || s2 != NULL)
-	{
-		ptr = malloc(sizeof(s1) + sizeof(s2));
-		while (*(s1 + i) != '\0')
-		{
-			ptr[i] = s1[i];
-			i++;
-		}
-		while (*(s2 + j) != '\0')
-		{
-			ptr[i] = s2[j];
-			i++;
-			j++;
-		}
-		ptr[i] = '\0';
-		return (ptr);
-	}
-	else
-	{
-		return (NULL);
-	}
-	return (NULL);
+char *ptr;
+int i;
+int j;
+int len;
+int len;
+len1 = 0;
+len2 = 0;
+i = 0;
+j = 0;
+while (*(s2 + len2) != '\0')
+{
+len2++;
+}
+while (*(s1 + len1) != '\0')
+{
+len1++;
+}
+if (s1 != NULL || s2 != NULL)
+{
+ptr = malloc(((len1 + len2) *sizeof(char)) + 1);
+while (*(s1 + i) != '\0')
+{
+ptr[i] = s1[i];
+i++;
+}
+while (*(s2 + j) != '\0')
+{
+ptr[i] = s2[j];
+i++;
+j++;
+}
+ptr[i] = '\0';
+return (ptr);
+}
+else
+{
+return (NULL);
+}
+return (NULL);
 }
