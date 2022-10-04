6f0ff3758fb6e7206744b8a0f5e5181a2bb09608
devviv <devviv>
2022-10-04 13:58:11 -0700

    task 1 and 2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,5 +10,20 @@
 
 char *_strdup(char *str)
 {
+	char *ptr;
+	int i;
 
+	i = 0;
+	if (str != NULL)
+	{
+		ptr = malloc(sizeof(str));
+		while (*(str + i) != '\0')
+		{
+			ptr[i] = str[i];
+			i++;
+		}
+		ptr[i] = '\0';
+		return (ptr);
+	}
+	return (0);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,5 +11,28 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	char *ptr;
+	int i;
+	int j;
 
+	i = 0;
+	j = 0;
+	if (s1 != NULL || s2 != NULL)
+	{
+		ptr = malloc(sizeof(s1) + sizeof(s2));
+		while (*(s1 + i) != '\0')
+		{
+			ptr[i] = s1[i];
+			i++;
+		}
+		while (*(s2 + j) != '\0')
+		{
+			ptr[i] = s2[j];
+			i++;
+			j++;
+		}
+		ptr[i] = '\0';
+		return (ptr);
+	}
+	return (0);
 }
