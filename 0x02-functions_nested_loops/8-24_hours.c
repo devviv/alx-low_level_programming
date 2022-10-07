ca7de95f55028a409e138cce1defc13a209cf352
devviv <devviv>
2022-10-07 05:53:51 -0700

    task 8 0x02-fun

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * jack_bauer - thi program prints every minute
@@ -6,45 +7,28 @@
  */
 void jack_bauer(void)
 {
-int i, j, k, l;
-i = j = k = l = 0;
-for (i = 0; i < 3; i++)
-{
-if (i < 2)
-{
-for (j = 0; j < 10; j++)
-{
-for (k = 0; k < 6; k++)
-{
-for (l = 0; l < 10; l++)
-{
-_putchar(i + '0');
-_putchar(j + '0');
-_putchar(':');
-_putchar(k + '0');
-_putchar(l + '0');
-_putchar('\n');
-}
-}
-}
-}
-else
-{
-for (j = 0; j < 4; j++)
-{
-for (k = 0; k < 6; k++)
-{
-for (l = 0; l < 10; l++)
-{
-_putchar(i + '0');
-_putchar(j + '0');
-_putchar(':');
-_putchar(k + '0');
-_putchar(l + '0');
-_putchar('\n');
-}
-}
-}
-}
-}
+	int i, j, k, l, n, m;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					if (i == 2 && (j >= 4 && j <= 9))
+					{
+						continue;
+					}
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
