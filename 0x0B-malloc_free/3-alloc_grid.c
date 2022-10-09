1a784ccc18546f6ba839a0a5dcd5cf0f9582a7d2
devviv <devviv>
2022-10-09 15:09:01 -0700

    changes

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,5 +11,21 @@
 
 int **alloc_grid(int width, int height)
 {
-
+int **ar;
+int i;
+int j;
+i = 0;
+if (width > 0 && height > 0)
+{
+*ar = (int *)malloc((height + width) * sizeof(int));
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+ar[i][j] = 0;
+}
+}
+return (ar);
+}
+return (0);
 }
--- 0x0C-more_malloc_free/3-arrray_range.c
+++ 0x0C-more_malloc_free/3-arrray_range.c
@@ -11,21 +11,4 @@
 
 int *array_range(int min, int max)
 {
-int **ar;
-int i;
-int j;
-i = 0;
-if (width > 0 && height > 0)
-{
-*ar = (int *)malloc((height + width) * sizeof(int));
-for (i = 0; i < height; i++)
-{
-for (j = 0; j < width; j++)
-{
-ar[i][j] = 0;
-}
-}
-return (ar);
-}
-return (0);
 }
