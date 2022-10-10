96b0b83abeda9dce33f6c38c96534cf3dbfd397f
devviv <devviv>
2022-10-10 15:25:31 -0700

    all

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,7 +14,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d = malloc(sizeof(struct dog));
 d->name = name;
 d->age = age;
 d->owner = owner;
