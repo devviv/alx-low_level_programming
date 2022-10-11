d37386d05cfa24040c63751e7686148ea5ccd54a
devviv <devviv>
2022-10-10 17:07:37 -0700

    correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,8 +16,8 @@ char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-typedef struct dog dog_t;
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG */
