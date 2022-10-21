511ffee8d3becba1ce09dbdca5f2d363085c6fba
devviv <devviv>
2022-10-20 19:19:29 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * before_main - execute before main
+ *
+ * Return: nothing
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
