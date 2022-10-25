d45cf9fd22bc8c826185aecfbfec371fae6ee81b
devviv <devviv>
2022-10-24 22:37:21 -0700

    correction

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,10 @@
-#ifndef LIST_H_
-#define LIST_H_
+#ifndef LIST_H
+#define LIST_H
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -12,11 +13,13 @@
  * Description: singly linked list node structure
  *
  */
+
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
