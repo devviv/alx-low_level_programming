e48bf25b535f36802b5651b189ae88e8901cf087
devviv <devviv>
2022-10-26 02:13:16 -0700

    correction

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,11 +4,26 @@
 #include "lists.h"
 /**
 *add_nodeint_end - add a node at ent of list
-*@h : the list
+*@head : the list
 *@n : the element to be added
 *Return: the list
 */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t **element;
 
+	(*element)->n = n;
+	(*element)->next = NULL;
+
+	listint_t **help;
+
+	help = head;
+
+	while (help != NULL)
+	{
+		(*help) = (*help)->next;
+	}
+	(*help)->next = (*element);
+
+	return (*head);
 }
